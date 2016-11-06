64b453b5fc9da6da2b939a270baabe7e218170e6
Jess <jess.harrelson0707@gmail.com>
2016-11-06 16:42:46 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth(){
+  return true
+}
+
+function sayHey(){
+  return "Hey!"
+}
+
+function sayHeyFriend(name){
+  return `Hey, ${name}!`
+}
+
+function favoriteIceCream(ic){
+  return `I love ${ic}`
+}
+
+function shouting(string){
+  return string.toUpperCase()
+}
+
+function roundDown(num){
+  return Math.floor(num)
+}
